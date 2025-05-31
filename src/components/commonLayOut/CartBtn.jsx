f68c2a3c3b6bf47e290dab12395130f5f60df830
Githubshakil <shakilmahmud007@gmail.com>
2025-06-01 01:12:47 +0600

    update

--- src/components/commonLayOut/CartBtn.jsx
+++ src/components/commonLayOut/CartBtn.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Cart from '../../icons/Cart'
+import React from "react";
+import Cart from "../../icons/Cart";
 
 const CartBtn = () => {
   return (
     <>
-        <button className='flex items-center gap-2.5 border border-[#000] py-4 px-[37px] rounded-[52px] cursor-pointer mt-[21px]'>
-           <Cart /> 
-           Add to Cart
-        </button>
+      <button className="flex items-center gap-2.5 border border-[#000] py-4 px-[37px] rounded-[52px] cursor-pointer mt-[21px] transform hover:scale-105 transition-all duration-300 ease-in-out">
+        <Cart />
+        Add to Cart
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default CartBtn
+export default CartBtn;
