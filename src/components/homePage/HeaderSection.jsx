18d0066ea562be4bdc5df2455aa780d5bd591390
Githubshakil <shakilmahmud007@gmail.com>
2025-05-31 14:10:45 +0600

    setup done

--- src/components/homePage/HeaderSection.jsx
+++ src/components/homePage/HeaderSection.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Container from '../commonLayOut/Container'
 
 const HeaderSection = () => {
   return (
     <>
-    <h1>Header Section</h1>
+    <Container>
+        <div className='max-w-[1240px] mx-auto'>
+            <h1 className='text-3xl font-bold'>Header Section</h1>
+            <p className='text-lg mt-2'>This is the header section of the page.</p>
+        </div>
+    </Container>
+       
+    
     </>
   )
 }
