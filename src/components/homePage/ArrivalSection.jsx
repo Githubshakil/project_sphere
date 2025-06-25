cddccf8619902bacf9e966b3252d66333fed80e2
Githubshakil <shakilmahmud007@gmail.com>
2025-06-26 01:43:48 +0600

    update

--- src/components/homePage/ArrivalSection.jsx
+++ src/components/homePage/ArrivalSection.jsx
@@ -8,26 +8,29 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useState } from "react";
 
-
-function NextRightArrow(props){
-  return(
-    <div onClick={props.onClick} className={` absolute -top-[100px] right-0 flex items-center justify-center w-[55px] h-[55px] rounded-full border border-[#000] hover:bg-[#AFE638] hover:border-[#AFE638] transition-all duration-300 cursor-pointer `}>
-                <ArrowRight />
-              </div>
-  )
+function NextRightArrow(props) {
+  return (
+    <div
+      onClick={props.onClick}
+      className={` absolute -top-[100px] right-0 flex items-center justify-center w-[55px] h-[55px] rounded-full border border-[#000] hover:bg-[#AFE638] hover:border-[#AFE638] transition-all duration-300 cursor-pointer `}
+    >
+      <ArrowRight />
+    </div>
+  );
 }
-function PrevNextArrow(props){
-  return(
-   <div onClick={props.onClick} className=" absolute -top-[100px] right-20 flex items-center justify-center w-[55px] h-[55px] rounded-full border border-[#000] hover:bg-[#AFE638] hover:border-[#AFE638] transition-all duration-300 cursor-pointer ">
-                <ArrowLeft />
-              </div>
-  )
+function PrevNextArrow(props) {
+  return (
+    <div
+      onClick={props.onClick}
+      className=" absolute -top-[100px] right-20 flex items-center justify-center w-[55px] h-[55px] rounded-full border border-[#000] hover:bg-[#AFE638] hover:border-[#AFE638] transition-all duration-300 cursor-pointer "
+    >
+      <ArrowLeft />
+    </div>
+  );
 }
-
 
 const ArrivalSection = () => {
-  const [selectedIndex, setSelectedIndex] = useState(null)
-  
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   var settings = {
     dots: false,
@@ -35,7 +38,7 @@ const ArrivalSection = () => {
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    nextArrow: <NextRightArrow/>,
+    nextArrow: <NextRightArrow />,
     prevArrow: <PrevNextArrow />,
     autoplay: true,
     autoplaySpeed: 2500,
@@ -88,32 +91,42 @@ const ArrivalSection = () => {
 
           <div className=" flex justify-between  gap-[76px]">
             <div className="mt-8 border-l border-[#E2E2E2] h-[308px]  relative">
-             
-              <span
-                className="absolute left-0 w-[2px] bg-[#5e1ee5] transition-all duration-300"
-                style={{
-                  top: `${selectedIndex !== null ? 10 + selectedIndex * 62 : 10}px`,
-                  height: "43px",
-                }}
-                
-                />
 
               <div className=" w-[250px] absolute top-[17px] cursor-pointer">
                 {arrivalItems.map((item, index) => (
-                  <ArrivalItems
-                    key={index}
-                    arrivalText={item.arrivalText}
-                    arrivalNumber={item.arrivalNumber}
-                    bg={selectedIndex === index ? "#5E1EE5" : item.bg}
-                    color={selectedIndex === index ? "#fff" : item.color}
-                    onClick={() => setSelectedIndex(index)}
-                  />
+                  <div className="flex items-center gap-[18px] mb-[30px] ml-6 "
+                  onClick={() => setSelectedIndex(index)}
+                  >
+                    <span
+                      className="absolute left-0 w-[2px] bg-[#5e1ee5] transition-all duration-300"
+                      style={{
+                        top: `${
+                          selectedIndex !== null ? -10 + selectedIndex * 62 : -10
+                        }px`,
+                        height: "43px",
+                      }}
+                    />
+                    <h6 className='text-[#2c2c2c] font-["Montserrat"] text-base font-medium leading-[130%] '>
+                      {item.arrivalText}
+                    </h6>
+                    <div
+                      className='w-[40px] h-[31px] text-[#fff] font-["Montserrat"]  bg-amber-400 flex items-center justify-center rounded-[8px] text-[15px] font-bold '
+                      style={{
+                        backgroundColor: item.bg ,
+                        color: item.color ,
+                      }}
+                    >
+                      {item.arrivalNumber}
+                    </div>
+                  </div>
+
+                  
                 ))}
               </div>
             </div>
 
             <div className="w-[940px]  ">
-              <Slider {...settings} >
+              <Slider {...settings}>
                 <ArrivalBox
                   loveIcon={true}
                   extraRight={true}
